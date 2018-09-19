3cdb0180e9d468bf14e1738db1881391014c434a
Martin Wangen <wangen93@hotmail.com>
2018-09-19 17:26:46 +0200

    Luftmotstand "ferdig" fil

--- Oppgave 5/Luftmotstand.py
+++ Oppgave 5/Luftmotstand.py
@@ -3,8 +3,10 @@ import math as m
 Cd = 0.75
 
 def tetthet(h):
+    if h < 0:
+        raise ValueError('Høyden kan ikke være negativ (under havnivå)')
     # troposfæren
-    if h < 11000:
+    if h < 11000 & h>0:
         T = 288.19 - 0.00649 * h
         p = 101.29 * (T / 288.08) ** 5.256
     # Nedre stratosfære
@@ -12,7 +14,7 @@ def tetthet(h):
         T = 216.69
         p = 127.76 * np.exp(-0.000157 * h)
     # Øvre stratosfære
-    else:
+    elif h > 25000:
         T = 141.94 + 0.00299 * h
         p = 2.488 * (T / 216.6) ** -11.388
     return (p / T) * 3.4855
