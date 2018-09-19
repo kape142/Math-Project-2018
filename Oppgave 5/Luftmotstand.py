4a7d5eb0b9f863fa2b01d68b6a0636631f5ec690
matseda <matseda@stud.ntnu.no>
2018-09-19 14:52:30 +0200

    Endret oppg5

--- Oppgave 5/Luftmotstand.py
+++ Oppgave 5/Luftmotstand.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+Cd = 0.75
 
 def tetthet(h):
     # troposfæren
@@ -7,16 +7,16 @@ def tetthet(h):
         T = 288.19 - 0.00649 * h
         p = 101.29 * (T / 288.08) ** 5.256
     # Nedre stratosfære
-    if h > 11000 & h < 25000:
+    elif h > 11000 & h < 25000:
         T = 216.69
         p = 127.76 * np.exp(-0.000157 * h)
     # Øvre stratosfære
-    if h > 25000:
+    else:
         T = 141.94 + 0.00299 * h
         p = 2.488 * (T / 216.6) ** -11.388
-    return p / T
+    return (p / T) * 3.4855
 
 
 # CD er aerodynamisk egenskap, h er høyde, A er areal gitt et tverrsnit og v e fart.
-def luftmotstand(cd, h, A, v):
-    return 0.5 * cd * tetthet(h) * A * v ** 2
+def luftmotstand(h, A, v):
+    return 0.5 * Cd * tetthet(h) * A * v ** 2
